1e029aeb322be4b0faa48ff3b7d9bd10dbcb1748
Setiadi Prawiryo Moeljadi <piyoo86@gmail.com>
2025-11-01 10:33:50 +0700

    fix: reduce redraw when opening toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -15,6 +15,24 @@ return {
         vim.defer_fn(function()
           vim.cmd("startinsert!")
         end, 0)
+
+        -- strip visual noise
+        vim.cmd([[
+          setlocal nonumber norelativenumber
+          setlocal signcolumn=no
+        ]])
+
+        -- hide statusline only for this win
+        vim.b._tt_prev_laststatus = vim.o.laststatus
+        vim.o.laststatus = 0
+      end,
+      on_close = function(term)
+        if vim.b._tt_prev_laststatus ~= nil then
+          vim.o.laststatus = vim.b._tt_prev_laststatus
+          vim.b._tt_prev_laststatus = nil
+        else
+          vim.o.laststatus = 3 -- or 2, whatever you use globally
+        end
       end,
       float_opts = {
         border = "curved",
