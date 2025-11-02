099befca37b508b07337b8ef770da117554ab883
Setiadi Prawiryo Moeljadi <piyoo86@gmail.com>
2025-11-02 14:46:41 +0700

    feat: add render-markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    event = "VeryLazy",
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
+}
